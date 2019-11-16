772366239976d3cfa71309745bb95e654a8565fc
Seb-lg <seb.le.gall@free.fr>
2019-11-16 01:18:59 +0000

    adding assets and simple test

--- Main.cpp
+++ Main.cpp
@@ -6,7 +6,63 @@
 #include <string>
 #include <Ecs.hpp>
 
-int main(int ac, char** av) {
+#include <irrlicht.h>
+using namespace irr;
+
+using namespace core;
+using namespace scene;
+using namespace video;
+using namespace io;
+using namespace gui;
+
+int main() {
+	IrrlichtDevice *device =
+		createDevice( video::EDT_SOFTWARE, dimension2d<u32>(640, 480), 16,
+			      false, false, false, 0);
+
+	if (!device)
+		return 1;
+
+	device->setWindowCaption(L"Hello World! - Irrlicht Engine Demo");
+	IVideoDriver* driver = device->getVideoDriver();
+	ISceneManager* smgr = device->getSceneManager();
+	IGUIEnvironment* guienv = device->getGUIEnvironment();
+
+	guienv->addStaticText(L"Hello World! This is the Irrlicht Software renderer!",
+			      rect<s32>(10,10,260,22), true);
+
+	IAnimatedMesh* mesh = smgr->getMesh("/home/seb/Sandbox/asset/sydney.md2");
+	if (!mesh)
+	{
+		device->drop();
+		return 1;
+	}
+	IAnimatedMeshSceneNode* node = smgr->addAnimatedMeshSceneNode( mesh );
+
+	if (node)
+	{
+		node->setMaterialFlag(EMF_LIGHTING, false);
+		node->setMD2Animation(scene::EMAT_STAND);
+		node->setMaterialTexture( 0, driver->getTexture("/home/seb/Sandbox/asset/sydney.bmp") );
+	}
+
+	smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
+
+	while(device->run())
+	{
+		driver->beginScene(true, true, SColor(255,100,101,140));
+
+		smgr->drawAll();
+		guienv->drawAll();
+
+		driver->endScene();
+	}
+
+	device->drop();
+	return 0;
+}
+
+/*int main(int ac, char** av) {
 	std::cout << "Hello World !" << std::endl;
 
 	auto ecs = Ecs::get();
@@ -16,4 +72,4 @@ int main(int ac, char** av) {
 
 	auto strings = ecs.getComponentMap<std::string>();
 	std::cout << strings[id] << std::endl;
-}
+}*/
