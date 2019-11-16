692caaff03845c4d6d7be40ee459790c88eeeb97
Seb-lg <seb.le.gall@free.fr>
2019-11-16 03:37:02 +0000

    loop fixed qt 60 fps

--- Main.cpp
+++ Main.cpp
@@ -7,6 +7,9 @@
 #include <Ecs.hpp>
 
 #include <irrlicht.h>
+#include <chrono>
+#include <Time.hpp>
+#include <thread>
 #include "component/AnimatedMeshScene.hpp"
 
 using namespace irr;
@@ -19,20 +22,26 @@ using namespace gui;
 
 int main() {
 	auto ecs = Ecs::get();
+	long time;
 
 	auto id = ecs::Entity::getId();
 	ecs.addComponent<AnimatedMeshScene>(id, "./asset/sydney.md2", "./asset/sydney.bmp");
 
 	ecs.smgr->addCameraSceneNode(0, vector3df(0,30,-40), vector3df(0,5,0));
 
-	while(ecs.device->run())
-	{
+	ecs.addUpdate(100, [&ecs](){
 		ecs.driver->beginScene(true, true, SColor(255,100,101,140));
-
 		ecs.smgr->drawAll();
 		ecs.guienv->drawAll();
-
 		ecs.driver->endScene();
+	});
+
+	while(ecs.device->run()) {
+		time = ecs::Time::get(TimeUnit::MicroSeconds);
+		ecs.update();
+
+		auto x = static_cast<unsigned int>(16666 - (ecs::Time::get(TimeUnit::MicroSeconds) - time) > 0 ? 16666 - (ecs::Time::get(TimeUnit::MicroSeconds) - time) : 0);
+		std::this_thread::sleep_for(std::chrono::microseconds(x));
 	}
 
 	ecs.device->drop();
--- component/AnimatedMeshScene.hpp
+++ component/AnimatedMeshScene.hpp
@@ -9,23 +9,21 @@
 
 struct AnimatedMeshScene {
 	AnimatedMeshScene(std::string meshPath, std::string texturePath) {
-		this->mesh = Ecs::get().smgr->getMesh(meshPath.c_str());
-		if (!this->mesh)
-		{
+		this->mesh = std::shared_ptr<IAnimatedMesh>(Ecs::get().smgr->getMesh(meshPath.c_str()));
+		if (!this->mesh) {
 			Ecs::get().device->drop();
 		}
-		this->node = Ecs::get().smgr->addAnimatedMeshSceneNode(this->mesh);
 
-		if (node)
-		{
-			node->setMaterialFlag(EMF_LIGHTING, false);
-			node->setMD2Animation(scene::EMAT_STAND);
-			node->setMaterialTexture( 0, Ecs::get().driver->getTexture(texturePath.c_str()) );
+		this->node = std::shared_ptr<IAnimatedMeshSceneNode>(Ecs::get().smgr->addAnimatedMeshSceneNode(this->mesh.get()));
+		if (this->node) {
+			this->node->setMaterialFlag(EMF_LIGHTING, false);
+			this->node->setMD2Animation(scene::EMAT_STAND);
+			this->node->setMaterialTexture( 0, Ecs::get().driver->getTexture(texturePath.c_str()) );
 		}
 	}
 
-	IAnimatedMesh* mesh;
-	IAnimatedMeshSceneNode* node;
+	std::shared_ptr<IAnimatedMesh> mesh;
+	std::shared_ptr<IAnimatedMeshSceneNode> node;
 };
 
 
